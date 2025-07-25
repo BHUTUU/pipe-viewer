aa50fc6632d34320f2d37e1a6abfe0226bf51d47
Suman Kumar <raj259942@gmail.com>
2025-07-25 06:22:29 +0530

    updated for win64

--- pv.c
+++ pv.c
@@ -1,5 +1,35 @@
+#if defined(_WIN32) || defined(_WIN64)
 #include <stdio.h>
 #include <stdlib.h>
+
+ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
+    size_t pos = 0;
+    int c;
+
+    if (*lineptr == NULL || *n == 0) {
+        *n = 128;
+        *lineptr = malloc(*n);
+        if (*lineptr == NULL) return -1;
+    }
+
+    while ((c = fgetc(stream)) != EOF) {
+        if (pos + 1 >= *n) {
+            *n *= 2;
+            char *new_ptr = realloc(*lineptr, *n);
+            if (!new_ptr) return -1;
+            *lineptr = new_ptr;
+        }
+        (*lineptr)[pos++] = c;
+        if (c == '\n') break;
+    }
+
+    if (pos == 0 && c == EOF) return -1;
+    (*lineptr)[pos] = '\0';
+    return pos;
+}
+#endif
+// #include <stdio.h>
+// #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
